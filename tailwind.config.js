97364c430b6d1e38acc25f2fc3557ebbd57be4a2
nadilakencana <nadilakencana1107@gmail.com>
2025-01-23 15:25:41 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3A3A3A',
+        secondary: '#D9D9D9',
+      
+
+      },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      }  
+    },
+  },
+  plugins: [],
+}
+
